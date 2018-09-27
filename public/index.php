6eba411f25b1f4e68e57c58b4d5b463248c05ba2
shaun.xu <shauns.xu@chofn.com>
2018-09-27 20:36:16 +0800

    使用composer

--- public/index.php
+++ public/index.php
@@ -5,6 +5,5 @@
  * @author     Shaun.Xu
  * @since      2018/1/12 14:41
  */
-//require_once ROOT . "/zenphp/Bootstrap.php";
 include dirname(dirname(__FILE__))."/vendor/autoload.php";
 \zenphp\Bootstrap::run();
